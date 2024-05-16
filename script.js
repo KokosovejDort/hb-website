0d9936060bd7be2295441559bb4efd9d2840cc18
KokosovejDort <149147141+KokosovejDort@users.noreply.github.com>
2024-05-16 16:56:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
 const questions = [
-    "Question_1",
-    "Question_2",
-    "Question_3",
-    "Question_4"
+    "Хочешь избавиться от человечества?",
+    "Мама: Андрей, ты и мной пожертвуешь, тебе меня не жаль?!",
+    "YSL: мы предлагаем 3-годичный контракт с дальнейшим продлением и с переездом в Токио. Этим тоже жертвуешь?",
+    "Мы (самые главные депутата мира) против твоего решения, ты уверен, что сможешь пойти против нас?"
+];
+
+const endMessages = [
+    "Пффф, быстро сдался.",
+    "Пффф, быстро сдался.",
+    "Пффф, ну ты быстро сдался.",
+    "Пффф, ну как ты мог тут сдаться."
 ];
 
 let currentQuestionIndex = 0;
+
+function startGame() {
+    document.getElementById('startButton').style.display = 'none';
+    document.getElementById('yesButton').style.display = 'inline-block';
+    document.getElementById('noButton').style.display = 'inline-block';
+    nextQuestion();
+}
 
 function nextQuestion() {
     if (currentQuestionIndex < questions.length) {
         document.getElementById("question").textContent = questions[currentQuestionIndex];
     } else {
-        endGame();
+        document.getElementById("question").textContent = "От Саши: это всего лишь игра, но поздравляю";
+        document.getElementById('yesButton').style.display = 'none';
+        document.getElementById('noButton').style.display = 'none';
+        document.getElementById('destroyButton').style.display = 'none';
     }
 }
 
 function yesClicked() {
     currentQuestionIndex++;
-    nextQuestion();
+    if (currentQuestionIndex === questions.length) {
+        document.getElementById("question").textContent = questions[currentQuestionIndex - 1];
+        document.getElementById('yesButton').style.display = 'none';
+        document.getElementById('noButton').style.display = 'none';
+        document.getElementById('destroyButton').style.display = 'inline-block';
+    } else {
+        nextQuestion();
+    }
 }
 
 function noClicked() {
-    currentQuestionIndex++;
-    nextQuestion();
+    document.getElementById("question").textContent = endMessages[currentQuestionIndex];
+    document.getElementById('yesButton').style.display = 'none';
+    document.getElementById('noButton').style.display = 'none';
 }
 
-function endGame() {
-    document.getElementById("question").textContent = "Thank you for playing!";
-    document.querySelector(".buttons").style.display = "none";
+function destroyClicked() {
+    document.getElementById("question").textContent = "От Саши: это всего лишь игра, но поздравляю";
+    document.getElementById('destroyButton').style.display = 'none';
 }
-
-nextQuestion();
